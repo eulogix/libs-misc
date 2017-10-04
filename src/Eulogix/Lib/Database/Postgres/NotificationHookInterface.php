87ddb4bb4e7faf6fcc529aba2efebb0ee2ff2bae
pietrobaricco <pietro.baricco@gmail.com>
2017-10-04 19:45:09 +0200

    fix

--- src/Eulogix/Lib/Database/Postgres/NotificationHookInterface.php
+++ src/Eulogix/Lib/Database/Postgres/NotificationHookInterface.php
@@ -7,7 +7,12 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Eulogix\Lib\Database\Postgres;
+
+/**
+ * @author Pietro Baricco <pietro@eulogix.com>
+ */
 
 interface NotificationHookInterface
 {
