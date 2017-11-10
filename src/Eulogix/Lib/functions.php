23216e0f05cdd1c1c7f5088a50eb370885d744c9
pietrobaricco <pietro.baricco@gmail.com>
2017-11-10 12:14:52 +0100

    added  function

--- src/Eulogix/Lib/functions.php
+++ src/Eulogix/Lib/functions.php
@@ -236,3 +236,14 @@ function evaluate_in_lambda($phpCode, array $context=[], $asExpression = false) 
 
     return $ret_;
 }
+
+/**
+ * @param $bytes
+ * @param int $decimals
+ * @return string
+ */
+function readable_filesize($bytes, $decimals = 2) {
+    $size = array('B','kB','MB','GB','TB','PB','EB','ZB','YB');
+    $factor = floor((strlen($bytes) - 1) / 3);
+    return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+}
